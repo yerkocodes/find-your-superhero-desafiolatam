808150d2f30af240d3967ce9e4230546abc96a6f
Yerko Cortes Baeza <yerkodigo@gmail.com>
2021-07-18 23:39:42 -0400

    Card, CanvasJS and homepage completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,20 +27,69 @@ $(document).ready(function(){
 		    <div class="col-md-8">
 		      <div class="card-body">
 			<h3 class="card-title">${heroName}</h3>
-			<p class="card-text">${heroConnections}</p>
+			<p class="card-text"><b>Conexiones:</b> ${heroConnections}</p>
 			<ul class="list-group list-group-flush">
-			  <li class="list-group-item">${heroPublisher}</li>
-			  <li class="list-group-item">${heroOccupation}</li>
-			  <li class="list-group-item">${heroAppearance}</li>
-			  <li class="list-group-item">${heroHeight}</li>
-			  <li class="list-group-item">${heroWeight}</li>
-			  <li class="list-group-item">${heroAliases}</li>
+			  <li class="list-group-item"><b>Publicado por:</b> ${heroPublisher}</li>
+			  <li class="list-group-item"><b>Ocupación:</b> ${heroOccupation}</li>
+			  <li class="list-group-item"><b>Primera Aparición:</b> ${heroAppearance}</li>
+			  <li class="list-group-item"><b>Altura:</b> ${heroHeight}</li>
+			  <li class="list-group-item"><b>Peso:</b> ${heroWeight}</li>
+			  <li class="list-group-item"><b>Alianzas: </b> ${heroAliases}</li>
 			</ul>
 		      </div>
 		    </div>
 		  </div>
 		</div>
-	  `)
+	  `);
+
+
+	let estadisticas = [];
+
+	estadisticas.push({
+	  label: "Intelligence",
+	  y: data.powerstats.intelligence
+	});
+	estadisticas.push({
+	  label: "strength",
+	  y: data.powerstats.strength
+	});
+	estadisticas.push({
+	  label: "speed",
+	  y: data.powerstats.speed
+	});
+	estadisticas.push({
+	  label: "durability",
+	  y: data.powerstats.durability
+	});
+	estadisticas.push({
+	  label: "power",
+	  y: data.powerstats.power
+	});
+	estadisticas.push({
+	  label: "combat",
+	  y: data.powerstats.combat
+	});
+
+	let config = {
+	  animationEnabled : true,
+	  title: {
+	    text: "Estadisticas"
+	  },
+	  data: 
+	  [{
+	    type: "pie",
+	    startAngle: 25,
+	    toolTipContent: "<b>{label}</b>: {y}%",
+	    showInLegend: "true",
+	    legendText: "{label}",
+	    indexLabelFontSize: 16,
+	    indexLabel: "{label} - {y}%",
+	    dataPoints: estadisticas
+	  }]
+	}
+
+	let chart = new CanvasJS.Chart("heroStats", config);
+	chart.render();
 
       },
     })
